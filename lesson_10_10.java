85a092dddadead453b177d48cbab739c80b749fd
ColotVS <kolotilov98@mail.ru>
2024-02-04 15:11:51 +0700

    Экранирование символов, часть 2 Урок 10, задача 10

---
+++ lesson_10_10.java
@@ -0,0 +1,10 @@
+public class lesson_10_10 {
+    //Выведи на экран следующий текст в две строки:
+    //It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
+    //It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
+
+    public static void main(String[] args) {
+        System.out.println("It\'s a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.print("It\'s a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
